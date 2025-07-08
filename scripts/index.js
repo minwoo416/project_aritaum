1df4692765e8a80965f975bad29e3c7b9bef289d
minwoo <jmw041615@gmail.com>
2025-07-08 15:23:58 +0900

    뉴 완성..?

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
+const wishListBtn = document.querySelectorAll('main .wishlist_plus');
+wishListBtn.forEach((obj)=>{
+    obj.addEventListener('click',()=>{
+        const img = obj.querySelector('img');/* 버튼 안의 이미지 */
+
+        if(img.src.includes('favorite-2.png')){
+            img.src = './images/favorite-1.png';
+        }else{
+            img.src = './images/favorite-2.png';
+        }
+    })
+})
+console.log(wishListBtn)
+
+
+
+
+
+
+
+
+
 /* swiper */
 const bnr = new Swiper('.banner_container',{
     slidesPerView:'3.5',
     spaceBetween:20,
+    loop:true,
     pagination: {
-        el: ".swiper .banner_btn .swiper-pagination",
+        el: ".swiper .banner_btn .arrow_wrap .swiper-pagination",
         type: "fraction",
     },
     navigation: {
@@ -12,6 +35,14 @@ const bnr = new Swiper('.banner_container',{
     },
     scrollbar: {
         el: ".swiper .banner_btn .swiper-scrollbar",
-        hide: true,
+        hide: false,
+    },
+})
+
+const newProduct = new Swiper('.new_container .new_product_container',{
+    slidesPerView:1,
+    navigation: {
+        prevEl: ".new_container .swiper-button-prev",
+        nextEl: ".new_container .swiper-button-next",
     },
 })
